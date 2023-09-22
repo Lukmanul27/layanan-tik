281197c97f90deed412768319eba1bd2775c03b1
Lukmanul27 <2006079@itg.ac.id>
2023-09-22 11:23:51 +0700

    up

--- resources/views/pengajuan/index.blade.php
+++ resources/views/pengajuan/index.blade.php
@@ -20,19 +20,19 @@
                             <th></th>
                         </thead>
                         <tbody>
-                            @foreach($pengajuan->sortByDesc('updated_at') as $data)
+                            @foreach($pengajuan->sortByDesc('created_at') as $data)
                             <tr>
                                 <td>{{$loop->iteration}}.</td>
                                 <td>
                                     {{ \App\Models\User::find($data->user_id)->name }}
                                 </td>
                                 <td>{{ \App\Models\Pelayanan::find($data->pelayanan_id)->nama }}</td>
-                                <td>{{ $data->updated_at->format('d-m-Y') }}</td>
+                                <td>{{ $data->created_at->format('d-m-Y') }}</td>
                                 <td>{{ $data->status }}</td>
                                 <td>
                                     <div class="btn-group" role="group">
                                         <button type="button" class="btn btn-outline-primary rounded-pill btn-sm"
-                                            data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+                                            data-bs-toggle="modal" data-bs-target="#modal-{{ $data->id }}">
                                             Detail
                                         </button>
                                     </div>
@@ -46,40 +46,32 @@
         </div>
     </div>
 </div>
+
+@foreach($pengajuan->sortByDesc('created_at') as $data)
 <div class="col-md-6 col-12">
     <div class="card">
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
-            aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-dialog-centered modal-dialog-scrollable"
-                role="document">
+        <div class="modal fade" id="modal-{{ $data->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-centered modal-dialog-scrollable" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalCenterTitle">Detail Pelayanan
-                            {{ \App\Models\Pelayanan::find($data->pelayanan_id)->nama }}
-                        </h5>
+                        <h5 class="modal-title" id="exampleModalCenterTitle">Detail Pelayanan {{ \App\Models\Pelayanan::find($data->pelayanan_id)->nama }}</h5>
                         <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                             <i data-feather="x"></i>
                         </button>
                     </div>
                     <div class="modal-body">
-                        <p>Pada Tanggal {{ $data->updated_at->format('d-m-Y') }},
-                            {{ \App\Models\User::find($data->user_id)->name }} Telah Mengajukan Perimintaan Layanan
-                            {{ \App\Models\Pelayanan::find($data->pelayanan_id)->nama }}, Dengan Detail Sebagai Berikut
-                        </p>
+                        <p>Pada Tanggal {{ $data->created_at->format('d-m-Y') }}, {{ \App\Models\User::find($data->user_id)->name }} Telah Mengajukan Permintaan Layanan {{ \App\Models\Pelayanan::find($data->pelayanan_id)->nama }}, Dengan Detail Sebagai Berikut</p>
+                        <!-- Tambahkan detail pengajuan lainnya sesuai kebutuhan -->
                     </div>
                     <div class="modal-footer">
                         <div class="btn-group" role="group">
                             <form action="{{ route('approve', $data->id) }}" method="POST">
                                 @csrf
-                                <button type="submit" class="btn btn-outline-success rounded-pill btn-sm"><i
-                                        class="bi bi-check2-circle"></i> Terima
-                                </button>
+                                <button type="submit" class="btn btn-outline-success rounded-pill btn-sm"><i class="bi bi-check2-circle"></i> Terima</button>
                             </form>
                             <form action="{{ route('disapprove', $data->id) }}" method="POST">
                                 @csrf
-                                <button type="submit" class="btn btn-outline-danger rounded-pill btn-sm"><i
-                                        class="bi bi-x-circle"></i> Tolak
-                                </button>
+                                <button type="submit" class="btn btn-outline-danger rounded-pill btn-sm"><i class="bi bi-x-circle"></i> Tolak</button>
                             </form>
                         </div>
                     </div>
@@ -88,6 +80,7 @@
         </div>
     </div>
 </div>
+@endforeach
 <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
